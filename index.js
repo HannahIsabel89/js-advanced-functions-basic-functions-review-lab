347dfd815f29fbcdc34d05e9dbc2e1d33896c186
Hannah <hannahwoodward@hotmail.com>
2021-10-04 22:26:21 +0800

    added a little

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Your code here
+// Lab question one
+function saturdayFun (activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun("play football");
+saturdayFun();
+
+// Lab question two
+let mondayWork = (activity="go to the office") => {
+    return `This Monday, I will ${activity}`;
+};
+mondayWork("go to the gym");
+
+// Lab question 3
+function wrapAdjective (style="*") {
+  return function (adj="special") {
+    return `You are ${style} ${adj} ${style}`
+  }
+};
+ // from answer guide
+let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`
+    }
+  }
+  
+  // Lab question 4
+  const Calculator = {
+    add: function(a,b) {
+      return a + b;
+    },
+    subtract: function(a,b) {
+      return a - b
+    },
+    multiply: function(a,b) {
+      return a * b
+    },
+    divide: function(a,b) {
+      return a / b;
+    }
+  }
